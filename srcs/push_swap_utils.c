cc96701a1a9e3d886235889b0104100e7537dd77
Mark Byrne <markbyrne477@gmail.com>
2024-12-13 11:09:40 +0000

    finished basic utils for push swap

--- srcs/push_swap_utils.c
+++ srcs/push_swap_utils.c
@@ -5,44 +5,86 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/13 08:37:04 by codespace         #+#    #+#             */
-/*   Updated: 2024/12/13 10:02:53 by codespace        ###   ########.fr       */
+/*   Created: 2024/12/13 08:57:50 by mbyrne            #+#    #+#             */
+/*   Updated: 2024/12/13 10:20:38 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int stack_len(t_stack *stack)
-{
-    int count;
+#include "push_swap.h"
 
-    if (!stack)
-        return (0);
-    count = 0;
-    while (stack)
-    {
-        stack = stack->next;
-        count++;
-    }
-    return (count);       
+int	stack_len(t_stack *stack)
+{
+	int	count;
+
+	if (!stack)
+		return (0);
+	count = 0;
+	while (stack)
+	{
+		stack = stack->next;
+		count++;
+	}
+	return (count);
 }
 
-t_stack *find_last(t_stack *stack)
+t_stack	*find_last(t_stack *stack)
 {
-    if (!stack)
-        return (NULL);
-    while (stack->next)
-        stack = stack->next;
-    return (stack);
+	if (!stack)
+		return (NULL);
+	while (stack->next)
+		stack = stack->next;
+	return (stack);
 }
 
-bool    stack_sorted(t_stack *stack)
+bool	stack_sorted(t_stack *stack)
 {
-    if(!stack)
-        return (1);
-    while (stack->next)
-    {
-        if (stack->nbr > stack->next->nbr)
-            return (false);
-        stack = stack->next;
-    }
-    return (true);
-}   
+	if (!stack)
+		return (1);
+	while (stack->next)
+	{
+		if (stack->nbr > stack->next->nbr)
+			return (false);
+		stack = stack->next;
+	}
+	return (true);
+}
+
+t_stack	*find_min(t_stack *stack)
+{
+	long	min;
+	t_stack	*min_stack;
+
+	if (!stack)
+		return (NULL);
+	min = LONG_MAX;
+	while (stack)
+	{
+		if (stack->nbr < min)
+		{
+			min = stack->nbr;
+			min_stack = stack;
+		}
+		stack = stack->next;
+	}
+	return (min_stack);
+}
+
+t_stack	*find_max(t_stack *stack)
+{
+	long	max;
+	t_stack	*max_stack;
+
+	if (!stack)
+		return (NULL);
+	max = LONG_MIN;
+	while (stack)
+	{
+		if (stack->nbr > max)
+		{
+			max = stack->nbr;
+			max_stack = stack;
+		}
+		stack = stack->next;
+	}
+	return (max_stack);
+}
