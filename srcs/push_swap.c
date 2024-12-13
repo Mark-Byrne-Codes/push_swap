cc35ba1b10194ed68292da71faeca26ac8fca89d
Mark Byrne <markbyrne477@gmail.com>
2024-12-13 07:02:17 +0000

    new logic for main

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -5,76 +5,34 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/12 09:42:09 by codespace         #+#    #+#             */
-/*   Updated: 2024/12/12 12:05:01 by codespace        ###   ########.fr       */
+/*   Created: 2024/12/13 08:57:50 by mbyrne            #+#    #+#             */
+/*   Updated: 2024/12/13 07:01:57 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void sort_stack(char **argv, t_stack *stack_a, t_stack *stack_b)
+int	main(int argc, char **argv)
 {
-    int i;
-    int j;
-    long num;
+	t_stack	*stack_a;
+	t_stack	*stack_b;
 
-    i = 1;
-    while (argv[i])
-    {
-        num = ft_atoi(argv[i]);
-        stack_b = malloc(sizeof(t_stack));
-        if (!stack_b)
-            ft_error();
-        stack_b->nbr = num;
-        stack_b->index = i - 1;
-        stack_b->next = NULL;
-        stack_b->prev = NULL;
-        stack_b->next = stack_a;
-        stack_a->prev = stack_b;
-        stack_a = stack_b;
-        i++;
-    }
-}
-t_stack *initialize_stack(char **argv, int argc)
-{
-    t_stack *stack_a = NULL;
-    t_stack *new_node;
-    long num;
-    int i = 0;
-
-    if (argc == 2)
-        argv = ft_split(argv[1], ' ');
-
-    while (argv[i])
-    {
-        num = ft_atoi(argv[i]);
-        new_node = malloc(sizeof(t_stack));
-        if (!new_node)
-        {
-            ft_free(argv);
-            return (NULL);
-        }
-        new_node->nbr = num;
-        new_node->index = -1;
-        new_node->next = stack_a;
-        if (stack_a)
-            stack_a->prev = new_node;
-        new_node->prev = NULL;
-        stack_a = new_node;
-        i++;
-    }
-    if (argc == 2)
-        ft_free(argv);
-    return (stack_a);
-}
-
-
-void push_swap(char **argv, long *stack_a, long *stack_b) {
-    *stack_a = 0;
-    *stack_b = 0;
-    sort_stack(argv, stack_a, stack_b);
-    if(is_sorted(stack_a, stack_b))
-        ft_putstr("OK\n");
-    else
-        ft_putstr("KO\n");
+	stack_a = NULL;
+	stack_b = NULL;
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
+		return (1);
+	else if (argc == 2)
+		argv = ft_split(argv[1], ' ');
+	init_stack_a(&stack_a, argv + 1);
+	if (stack_sorted(stack_a))
+	{
+		if (stack_len(stack_a) == 2)
+			sa(&stack_a, &stack_b);
+		else if (stack_len(stack_a) == 3)
+			sort_three(&stack_a);
+		else
+			sort_stack(&stack_a, &stack_b);
+	}
+	free_stack (&stack_a);
+	return (0);
 }
--- srcs/push_swap.h
+++ srcs/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 09:42:12 by codespace         #+#    #+#             */
-/*   Updated: 2024/12/12 09:42:12 by codespace        ###   ########.fr       */
+/*   Updated: 2024/12/13 06:49:32 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,21 @@
 # define PUSH_SWAP_H
 
 # include "../libft/libft.h"
+# include <stdbool.h>
 
 typedef struct s_stack
 {
-    long nbr;
-    long index;
+    int nbr;
+    int index;
+    bool cost;
+    bool above_median;
+    bool cheapest;
+    struct s_stack *target;
     struct s_stack *next;
     struct s_stack *prev;
 }   t_stack;
 
-void	ft_check_args(int argc, char **argv);
-void ft_error(void);
+//Handle Errors
+
 
 #endif
