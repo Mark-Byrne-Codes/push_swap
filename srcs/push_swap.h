5125f1a1316db2e76647e582faf8977a8073a2b0
Mark Byrne <markbyrne477@gmail.com>
2024-12-18 14:23:47 +0000

    created push a and push b operation

--- srcs/push_swap.h
+++ srcs/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 09:42:12 by codespace         #+#    #+#             */
-/*   Updated: 2024/12/18 06:36:43 by codespace        ###   ########.fr       */
+/*   Updated: 2024/12/18 10:10:57 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,14 @@ typedef struct s_stack
 }   t_stack;
 
 //Handle Errors
+int	valid_numbers(char *str);
+int	error_dup(t_stack *stack_a, int n);
+void	free_stack(t_stack **stack);
+void	free_errors(t_stack **stack_a);
 
+//Stack Initialization
+void	init_stack_a(t_stack **stack_a, char **argv);
+
+//
 
 #endif
